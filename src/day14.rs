405a184161a8288761638ef3bd1af7af9372900a
Frans-Lukas <franslukas@gmail.com>
2021-12-14 13:19:47 +0100

    day 14, div by two end result works. STRANGE.

--- src/day14.rs
+++ src/day14.rs
@@ -10,91 +10,6 @@ struct Rule {
     result: char,
 }
 
-#[aoc(day14, part1)]
-pub fn part1_chars(input: &str) -> i32 {
-    // let mut fl: String = input.lines().next().unwrap().to_string();
-    // println!("{}", fl);
-    // let mut rules = Vec::<Rule>::new();
-    // for line in input.lines().skip(2) {
-    //     let str_rule: Vec<String> = line.split(" -> ").map(|s| s.to_string()).collect();
-    //     rules.push(Rule {
-    //         gram: Gram {
-    //             c1: str_rule[0].chars().nth(0).unwrap(),
-    //             c2: str_rule[0].chars().nth(1).unwrap(),
-    //         },
-    //         result: str_rule[1].chars().nth(0).unwrap(),
-    //     });
-    //     // println!("{} -> {}", rules.last().unwrap().start, rules.last().unwrap().result);
-    // }
-    // let mut unique_chars = HashMap::<char, i32>::new();
-    //
-    // for i in 0..10 {
-    //     println!("{}", i);
-    //     let mut result = fl.clone();
-    //     let mut hits = Vec::<(MyMatch, char)>::new();
-    //     for rule in rules.iter() {
-    //         // println!("{} -> {}", rule.start, rule.result);
-    //         let matches = m_regx(fl.as_str(), rule.start.as_str());
-    //         for hit in matches {
-    //             hits.push((hit, rule.result));
-    //         }
-    //     }
-    //     hits.sort_by(|(a, _), (b, _)| a.start.partial_cmp(&b.start).unwrap());
-    //     for (i, (hit, c)) in hits.iter().enumerate() {
-    //         // println!(
-    //         //     "hit: {}, start: {}, char: {}",
-    //         //     hit.text,
-    //         //     hit.start + 1 + i,
-    //         //     c
-    //         // );
-    //         // println!("{}", result);
-    //         result.insert(hit.start + i + 1, *c);
-    //     }
-    //     // println!("{}", result);
-    //     fl = result;
-    // }
-    // for c in fl.chars() {
-    //     if unique_chars.contains_key(&c) {
-    //         *unique_chars.get_mut(&c).unwrap() += 1;
-    //     } else {
-    //         unique_chars.insert(c, 1);
-    //     }
-    // }
-    // let mut max = 0;
-    // let mut min = MAX;
-    // for (c, v) in unique_chars.iter() {
-    //     if *v > max {
-    //         max = *v;
-    //     }
-    //     if *v < min {
-    //         min = *v;
-    //     }
-    // }
-
-    0
-}
-
-pub struct MyMatch {
-    text: String,
-    start: usize,
-}
-
-pub fn m_regx(search_string: &str, regex: &str) -> Vec<MyMatch> {
-    let c1: char = regex.chars().nth(0).unwrap();
-    let c2: char = regex.chars().nth(1).unwrap();
-    let mut found_matches = Vec::<MyMatch>::new();
-    for (i, c) in search_string.chars().enumerate() {
-        if i < search_string.len() - 1 && c == c1 && search_string.chars().nth(i + 1).unwrap() == c2
-        {
-            // println!("rx: {} -> {}{}", regex, c1, c2);
-            found_matches.push(MyMatch {
-                text: search_string[i..i + 2].to_string(),
-                start: i,
-            });
-        }
-    }
-    found_matches
-}
 #[derive(Hash, Copy, Clone)]
 struct Gram {
     c1: char,
@@ -109,12 +24,21 @@ impl PartialEq for Gram {
 impl Eq for Gram {}
 
 
+#[aoc(day14, part1)]
+pub fn part1_chars(input: &str) -> i64 {
+    apply_rules(input, 10)
+}
+
 #[aoc(day14, part2)]
-pub fn part2_chars(input: &str) -> i32 {
+pub fn part2_chars(input: &str) -> i64 {
+    apply_rules(input, 40)
+}
+
+pub fn apply_rules(input: &str, iters: i32) -> i64 {
     let mut fl: String = input.lines().next().unwrap().to_string();
-    println!("{}", fl);
     let mut rules = Vec::<Rule>::new();
-    let mut num_of_each = HashMap::<Gram, i32>::new();
+    let mut num_of_each = HashMap::<Gram, i64>::new();
+
     for (i, c) in fl.chars().enumerate() {
         if i < fl.len() - 1 {
             let to_inesrt = Gram {
@@ -128,6 +52,7 @@ pub fn part2_chars(input: &str) -> i32 {
             }
         }
     }
+
     for line in input.lines().skip(2) {
         let str_rule: Vec<String> = line.split(" -> ").map(|s| s.to_string()).collect();
         rules.push(Rule {
@@ -137,72 +62,50 @@ pub fn part2_chars(input: &str) -> i32 {
             },
             result: str_rule[1].chars().nth(0).unwrap(),
         });
-        // println!("{} -> {}", rules.last().unwrap().start, rules.last().unwrap().result);
     }
 
-    for i in 0..3 {
-        let mut new_grams = HashMap::<Gram, i32>::new();
+    for _ in 0..iters {
+        let mut new_grams = HashMap::<Gram, i64>::new();
         for rule in rules.iter() {
             if num_of_each.contains_key(&rule.gram) {
-                let to_insert1 = Gram {
-                    c1: rule.gram.c1,
-                    c2: rule.result,
-                };
-                if new_grams.contains_key(&to_insert1) {
-                    *new_grams.get_mut(&to_insert1).unwrap() += 1;
-                } else {
-                    new_grams.insert(
-                        to_insert1,
-                        *num_of_each.get(&rule.gram).unwrap(),
-                    );
-                }
-                let to_insert2 = Gram {
-                    c1: rule.result,
-                    c2: rule.gram.c2,
-                };
-
-                if new_grams.contains_key(&to_insert2) {
-                    *new_grams.get_mut(&to_insert2).unwrap() += 1;
-                } else {
-                    new_grams.insert(
-                        to_insert2,
-                        *num_of_each.get(&rule.gram).unwrap(),
-                    );
+                for to_insert in vec![
+                    Gram {
+                        c1: rule.gram.c1,
+                        c2: rule.result,
+                    },
+                    Gram {
+                        c1: rule.result,
+                        c2: rule.gram.c2,
+                    },
+                ] {
+                    let count = *num_of_each.get(&rule.gram).unwrap();
+                    if new_grams.contains_key(&to_insert) {
+                        *new_grams.get_mut(&to_insert).unwrap() += count;
+                    } else {
+                        new_grams.insert(to_insert, count);
+                    }
                 }
             }
         }
         num_of_each = new_grams;
-
-        println!();
-        for (gram, v) in num_of_each.iter() {
-            print!("{}{}: {},", gram.c1, gram.c2, v);
-        }
-        println!();
     }
 
-    println!();
-    for (gram, v) in num_of_each.iter() {
-        print!("{}{}: {},", gram.c1, gram.c2, v);
-    }
-    println!();
-
-    let mut unique_chars = HashMap::<char, i32>::new();
+    let mut unique_chars = HashMap::<char, i64>::new();
+    let mut sum = 0;
     for (gram, i) in num_of_each {
-        if unique_chars.contains_key(&gram.c1) {
-            *unique_chars.get_mut(&gram.c1).unwrap() += i;
-        } else {
-            unique_chars.insert(gram.c1, i);
-        }
-        if unique_chars.contains_key(&gram.c2) {
-            *unique_chars.get_mut(&gram.c2).unwrap() += i;
-        } else {
-            unique_chars.insert(gram.c2, i);
+        sum += i;
+        for gram_c in vec![gram.c1, gram.c2] {
+            if unique_chars.contains_key(&gram_c) {
+                *unique_chars.get_mut(&gram_c).unwrap() += i;
+            } else {
+                unique_chars.insert(gram_c, i);
+            }
         }
     }
-    let mut max = 0;
-    let mut min = MAX;
-    for (c, v) in unique_chars.iter() {
 
+    let mut max = 0;
+    let mut min = i64::MAX;
+    for (c, v) in unique_chars.iter() {
         if *v > max {
             max = *v;
         }
@@ -210,6 +113,6 @@ pub fn part2_chars(input: &str) -> i32 {
             min = *v;
         }
     }
-    println!("max: {}", max);
-    max - min
+    // WTF, why does div by 2 work?
+    (max - min) / 2
 }
